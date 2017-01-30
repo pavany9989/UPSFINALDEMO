b14c52e47984112ec8be1a703dbf125786f7cd77
VDW7QYM <VDW7QYM@WKSP00075683.us.ups.com>
2017-01-30 03:59:15 -0500

    MAX UPdate

---
+++ J.bat
@@ -0,0 +1,3 @@
+cd C:\Users\VDW7QYM\workspace\CounterWebApp
+
+java -cp C:\Users\VDW7QYM\workspace\CounterWebApp\lib*;C:\Users\VDW7QYM\workspace\CounterWebApp\target\test-classes org.testng.TestNG testng.xml
---
+++ src/test/java/AMX/SelDemo.java
@@ -0,0 +1,10 @@
+package AMX;
+import org.testng.annotations.Test;
+public class SelDemo {
+    @Test
+	public void testdemo()
+	{
+		System.out.println("Welcome to Selenuium Testing");
+	}
+
+}
